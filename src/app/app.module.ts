4f84ec3ed1e4a791905f7778b4d1ec216dd75824
acpkhan85 <acpkhan85@gmail.com>
2017-09-17 20:45:05 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NewsComponent } from './news/news.component';
+import { EventComponent } from './event/event.component';
+import { HeadmasterComponent } from './headmaster/headmaster.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NewsComponent,
+    EventComponent,
+    HeadmasterComponent,
+    AboutusComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,RouterModule.forRoot([ { path: 'home', component: HomeComponent }, 
+    { path: 'aboutUs', component: AboutusComponent }, 
+    // { path: 'newsevent', component: EventNewsComponent }, 
+    // { path: 'admission', component: AdmissionComponent }, 
+    { path: '**', redirectTo: 'home', pathMatch: 'full' } ])
   ],
   providers: [],
   bootstrap: [AppComponent]
